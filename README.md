d99650eb3ff7e09f963b141ad9981e8294802258
marusoft <alimi.kmaruf@gmail.com>
2020-04-10 13:35:13 +0100

    re-arrange files and folder

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# user-validation-password
+# Task 1
+## Python First Task: Create Basic Calculator for Area of a Circle
+# Task 2
+## Python Task 2: User Data Validation
---
+++ task_1/task_1.py
@@ -0,0 +1,5 @@
+def calculate_area_of_circle():
+    r = float(input("Enter the value of radius:"))
+    PI = 3.142
+    return PI * r * r
+print(calculate_area_of_circle())   
