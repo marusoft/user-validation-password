c6522449b5f315b87e5267689d0393c04e7044a4
marusoft <alimi.kmaruf@gmail.com>
2020-04-10 11:44:45 +0100

    refactor to print user details

--- user_validation.py
+++ user_validation.py
@@ -17,39 +17,58 @@ def generate_employee_password(employee_details):
   length_to_generate = 5
   get_random_str_length_five = ''.join(random.choice(string.ascii_lowercase) 
             for i in range(length_to_generate))
-  employee_random_password = str(employee_details[0][0:2] + employee_details[1][-2:-1] + get_random_str_length_five)
+  employee_random_password = str(employee_details[0][0:2] + employee_details[1][-2:] + get_random_str_length_five)
   return employee_random_password
 
 
+state = True
+container = []
 # dict to store user data
-user_data = {}
-initial_user_key = 1
+# user_data = {}
+# initial_user_key = 1
 
-while True:
+while state:
   user_info = get_employee_details()
   
-  display_password = generate_employee_password( user_info)
+  display_password = generate_employee_password(user_info)
   print('Your password is: ' + str(display_password))
   
-  is_pass_ok = input(str('Are you satisfied with this password. If yes enter Yes If no, please supply password'))
+  is_pass_ok = input(str('Are you satisfied with this password. If yes enter Yes If no, enter No then please supply password: '))
+  
   pass_loop = True
   
   while pass_loop:
     if is_pass_ok == 'Yes':
       user_info.append(display_password)
-      user_data.get(user_info)
+      container.append(user_info)
+      # user_data.get(user_info)
       
       pass_loop = False
     else:
-      user_password = input(str('Enter password greater than or equal to 7'))
+      user_password = input(str('Enter password greater than or equal to 7: '))
       
       pass_len = True
+      
       while pass_len:
         if len(user_password) >= 7:
-          user_info.append(user_info)
+          user_info.append(user_password)
+          container.append(user_info)
           
-          user_data.get(user_info)
+          # user_data.append(user_info)
           
           pass_len = False
           
           pass_loop = False
+        else:
+          print('Your password is less than 7')
+          user_password = input(str('Enter password greater than or equal to 7: '))
+          
+          
+  new_employee = input(str('Would you like to enter a new employee? Yes or No: '))
+  if (new_employee == 'No'):
+    state = False
+    for item in container:
+      print(item)
+        
+  else:
+    state = True
